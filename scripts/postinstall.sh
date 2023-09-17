86b67decbcacbc392e1fd12e6479f03744679c77
NotACat1 <notacat.mail@gmail.com>
2023-09-18 00:46:34 +0400

    fix: исправлен путь для проверки инициализации `Hasky`

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-if [ ! -d ".husky" ]; then
+if [ ! -d "./.husky/_" ]; then
   echo "Инициализация husky...";
   npx husky install;
 fi
-
-echo "Инициализация husky...";
-npx husky install;
 
 chmod +x .husky/commit-msg;
 chmod +x .husky/pre-commit;
