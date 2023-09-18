310306a9d730b4bb71c59a0f90348a9eb23b3298
Bogdan <bogdan10050@gmail.com>
2023-09-18 14:35:29 +0300

    fix: исправить ошибки после ревью

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const projectRoot = path.resolve(__dirname);
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '',
+  },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    compress: true,
+    port: 8080,
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': projectRoot + '/src',
+      '@fonts': projectRoot + '/src/vendor/fonts/',
+    },
+    extensions: [
+      '.js',
+      '.json',
+      '.css',
+      '.scss',
+      '.sass',
+      '.svg',
+      '.png',
+      '.jpeg',
+      '.gif',
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/',
+      },
+      {
+        test: /\.(scss|sass|css)$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/pages/index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};
