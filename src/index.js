8da6ee77868ea479f392561efe395efd48ef0bea
Sveta Podsoblyaeva <sa.podsoblyaeva@yandex.ru>
2023-09-20 22:36:58 +0300

    fix: коммит на всякий случай, так как терминал говорит, что какие-то файлы не закоммитились

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './scss/index.scss';
-import {Slider} from "./components/Slider.js"
+import {Slider} from './components/Slider.js';
 
-const slider = document.querySelector('.slider')
+const slider = document.querySelector('.slider');
 
 const sliderItem = new Slider(slider);
 sliderItem.timer();
