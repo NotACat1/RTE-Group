8a7a41619c888887b2a1488f10d45b033af034e9
NotACat1 <notacat.mail@gmail.com>
2023-09-23 22:22:52 +0400

    feat: подключение скрипта header

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './scss/index.scss';
-import {Slider} from './components/Slider.js';
+import { Slider } from './components/Slider.js';
+import { header } from './components/script-header.js';
+header();
+
 
 const slider = document.querySelector('.slider');
 
