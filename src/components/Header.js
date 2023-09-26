f4e4c338ee6197a85768a918a6e2b0b930a7c07e
NotACat1 <notacat.mail@gmail.com>
2023-09-26 23:45:53 +0400

    style: убран предыдущий код

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,3 @@
-//export function header() {
-//  const listButton = document.querySelector('.header__list-btn');
-//  const headerPopup = document.querySelector('.header__nav');
-
-//  console.log(listButton);
-
-//  listButton.addEventListener('click', () => {
-//    listButton.classList.toggle('header__list-btn_open');
-//    listButton.classList.toggle('header__list-btn_close');
-
-//    headerPopup.classList.toggle('header__nav_open');
-//    headerPopup.classList.toggle('header__nav_close');
-//  });
-//}
-
 export default class header {
   constructor(elemHeader, { selectorListButton, selectorNav, classListOpen, classNavOpen, classNavClose }) {
     this.elemListButton = elemHeader.querySelector(selectorListButton);
